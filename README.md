7ddc1c723c7a18491fac7156fbdfd412aec37edd
Adam Forester <aforester@silverdogstudios.com>
2018-06-23 13:03:16 -0500

    Base Version

---
+++ README.md
@@ -0,0 +1,15 @@
+This script will search your object database for unused objects. It will output a deletion commands file to clean those up. Both SMS and MDS are supported in the same script.
+
+You have two options of use; NAME or UID.
+
+## Please be careful when using any API tool to modify your database. Be sure to verify all data ##
+
+
+## How to use ##
+ - cp script over to mgmt station (this script is intended to run directly on the mgmt station)
+ - execute ./script-name.sh
+  - script will ask for IP of SMS or Domain of MDS you wish to search
+ - Output will be in delete-unused-objects.txt
+  - delete-unused-objects.txt will have the mgmt_cli commands for deletion. If you want to execute it do the following;
+    - chmod 755 delete-unused-objects.txt
+    - ./delete-unused-objects.txt
---
+++ delete-unused-objects-name.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+printf "\nWhat is the IP address or Name of the Domain or SMS you want to check?\n"
+read DOMAIN
+
+printf "\nDetermining Number of Objects\n"
+total=$(mgmt_cli -r true -d $DOMAIN show objects --format json |jq '.total')
+printf "There are $total objects\n"
+
+printf "\nSearching for unused HOST objects. Depending on number of objects this can take a min.\n"
+for I in $(seq 0 500 $total)
+do
+mgmt_cli -r true -d $DOMAIN show unused-objects offset $I limit 500 --format json | jq --raw-output --arg q "'" '.objects[] | select(.type == "host") | (" delete host name " + $q + .name + $q)' >>delete-unused-hosts-tmp.txt
+done
+sed "s/^/mgmt_cli -r true -d $DOMAIN/" delete-unused-hosts-tmp.txt > delete-unused-objects.txt; rm *tmp*.txt
+
+printf "\nSearching for unused NETWORK objects. Depending on number of objects this can take a min.\n"
+for I in $(seq 0 500 $total)
+do
+mgmt_cli -r true -d $DOMAIN show unused-objects offset $I limit 500 --format json | jq --raw-output --arg q "'" '.objects[] | select(.type == "network") | (" delete network name " + $q + .name + $q)' >>delete-unused-networks-tmp.txt
+done
+sed "s/^/mgmt_cli -r true -d $DOMAIN/" delete-unused-networks-tmp.txt >> delete-unused-objects.txt; rm *tmp*.txt
+
+printf "\nSearching for unused GROUP objects. Depending on number of objects this can take a min.\n"
+for I in $(seq 0 500 $total)
+do
+mgmt_cli -r true -d $DOMAIN show unused-objects offset $I limit 500 --format json | jq --raw-output --arg q "'" '.objects[] | select(.type == "group") | (" delete group name " + $q + .name + $q)' >>delete-unused-groups-tmp.txt
+done
+sed "s/^/mgmt_cli -r true -d $DOMAIN/" delete-unused-groups-tmp.txt >> delete-unused-objects.txt; rm *tmp*.txt
+
+printf "Host Deletion Commands in delete-unused-objects.txt\n"
